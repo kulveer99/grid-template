3309dd6a4db1941dabc91714607216c8072a1d5b
kulveer99 <60896386+kulveer99@users.noreply.github.com>
2022-11-05 12:52:19 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <title>Template</title>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">Header</div>
+                <div class="sidebar">Sidebar</div>
+                <div class="main">Main</div>
+                <div class="content1">Content1</div>
+                <div class="content2">Content2</div>
+                <div class="content3">Content3</div>
+                <div class="footer">Footer</div>
+            </div>
+            <div>
+        </body>
+    </html>
